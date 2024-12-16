2825011e19bdbadf93ddca80463dc59575a90d34
Jack Arthur Harrhy <me@jackharrhy.com>
2024-12-16 14:38:50 -0330

    yayayyayayya

--- lib/ileah/consumer.ex
+++ lib/ileah/consumer.ex
@@ -6,7 +6,7 @@ defmodule ILeah.ExampleConsumer do
   def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
     case msg.content do
       "blow up" ->
-        Api.create_message(msg.channel_id, "ya ya ya ya blow up ya ya")
+        Api.create_message(msg.channel_id, String.duplicate("ya", Enum.random(3..5)) <> " blow up " <> String.duplicate("ya", Enum.random(2..3)))
       _ ->
         :ignore
     end
