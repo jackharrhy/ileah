3c2ac63ec6c48a2d260adcb2233e85e404126b2d
Jack Arthur Harrhy <me@jackharrhy.com>
2025-09-22 01:04:05 -0230

    slash command

--- lib/ileah/application.ex
+++ lib/ileah/application.ex
@@ -2,7 +2,7 @@ defmodule ILeah.Application do
   use Application
 
   def start(_type, _args) do
-    children = [ILeah.ExampleConsumer]
+    children = [ILeah.Bot]
 
     Supervisor.start_link(children, strategy: :one_for_one)
   end
---
+++ lib/ileah/commands/leah_say.ex
@@ -0,0 +1,71 @@
+defmodule ILeah.Bot.Commands.LeahSay do
+  alias ILeah.Voice
+  alias Nostrum.Api
+  alias Nostrum.Constants.InteractionCallbackType
+  alias Nostrum.Constants.ApplicationCommandOptionType
+
+  def definition do
+    %{
+      name: "leah-say",
+      description: "Have Leah say something using text-to-speech",
+      options: [
+        %{
+          type: ApplicationCommandOptionType.string(),
+          name: "text",
+          description: "Text for Leah to say",
+          required: true
+        }
+      ]
+    }
+  end
+
+  def handle(interaction) do
+    if ILeah.Bot.is_permitted(interaction) do
+      text = get_text_option(interaction)
+
+      case Voice.text_to_speech(text) do
+        %{status: 200, body: body} ->
+          Api.create_interaction_response(interaction, %{
+            type: InteractionCallbackType.channel_message_with_source(),
+            data: %{
+              files: [
+                %{
+                  name: "leah_say.mp3",
+                  body: body
+                }
+              ]
+            }
+          })
+
+        response ->
+          IO.puts("Error: Failed to convert text to speech: #{inspect(response)}")
+
+          Api.create_interaction_response(interaction, %{
+            type: InteractionCallbackType.channel_message_with_source(),
+            data: %{
+              content: "oh no, failed to convert text to speech"
+            }
+          })
+      end
+    else
+      Api.create_interaction_response(interaction, %{
+        type: InteractionCallbackType.channel_message_with_source(),
+        data: %{
+          content: "you cannot use this command, blow up"
+        }
+      })
+    end
+  end
+
+  defp get_text_option(interaction) do
+    case interaction.data.options do
+      nil ->
+        ""
+
+      options ->
+        Enum.find_value(options, "", fn option ->
+          if option.name == "text", do: option.value, else: nil
+        end)
+    end
+  end
+end
